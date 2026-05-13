4759ba10b4f56a3a2c6d037953e533b849a11bb2
Jimly23 <jimlyasidqi692@gmail.com>
2026-05-13 12:28:38 +0700

    Update db

--- api/config/config.local.php
+++ api/config/config.local.php
@@ -6,4 +6,4 @@ define('DB_HOST', 'localhost');
 define('DB_PORT', '3306');
 define('DB_NAME', 'speakon_db');
 define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_PASS', 'Hardiansyah_23');
